5d511f74fbd42fc2663998f55e32613bbbfb5fc2
LukeAlderson02523332 <lukelalderson@gmail.com>
2025-04-24 19:02:27 +0100

    Added a working ppo_learner function
    Convert to class at some point

--- learners.py
+++ learners.py
@@ -5,8 +5,9 @@ import torch.nn as nn
 from env import DiceGame, run_episode, update_network
 from matplotlib import pyplot as plt
 from torch.distributions import Categorical
-import gym
+import gymnasium as gym
 from scipy.stats import entropy
+import copy
 
 def reinforce_learner(env, params, device):
     
@@ -172,3 +173,169 @@ def a2c_learner(env, params, device):
     
     print("Finished!\n")    
     return reward_totals
+
+def ppo_learner(env, params, device):
+    
+    ''' 
+    Description: Accepts an environment and a parameter dictionary and applies
+    Advantage Actor Critic (A2C) to the provided environment.
+    
+    Returns: A list of returns for plotting
+    '''
+    
+    # Get hyperparameter values from the parameter dictionary
+    iterations = params.get('iterations')
+    lr = params.get('lr')
+    gamma = params.get('gamma')
+    T = params.get('rollout_len')
+    eps = params.get('eps')
+    
+    # Define action and state space sizes.
+    # State Space --> [Cart Pos, Cart Vel, Pole Pos, Pole Vel]
+    # Action Space --> [0 (left), 1 (right)]
+    action_space = env.action_space.n
+    state_space = env.observation_space.shape[0]
+    
+    # Create instances of actor (policy) and critic (value) networks, and
+    # corresponding optimizers.
+    policy_net = PolicyNetwork(state_space, action_space).to(device)
+    old_policy_net = copy.deepcopy(policy_net)
+    policy_optimizer = optim.Adam(policy_net.parameters(), lr=lr)
+    
+    value_net = ValueNetwork(state_space, 1).to(device)
+    value_optimizer = optim.Adam(value_net.parameters(), lr=lr)
+    
+    # Set loss function to be used and init other variables
+    mse_loss = nn.MSELoss()
+    reward_totals = []
+    global_steps = 0
+    print("\nStarting Training...")
+    state, info = env.reset()
+    state = torch.from_numpy(state).to(device)
+    
+    for n in range(iterations):
+        
+        buffer = perform_rollout(env, old_policy_net, value_net, T, device) # (50, )
+        
+        #return_to_go = get_returns(buffer["reward"], gamma) # (50, ) OLD
+        return_to_go = get_returns(buffer["reward"], buffer["dones"], gamma) # NEW
+        
+        rollout_states = torch.stack(buffer["state"]).to(device)
+        rollout_returns = torch.tensor(return_to_go).to(device)
+        rollout_actions = torch.tensor(buffer["action"]).to(device)
+        rollout_values = torch.tensor(buffer["values"]).to(device)
+        old_log_probs = torch.tensor(buffer["old_log_probs"]).to(device)
+        
+        advantage = rollout_returns - rollout_values
+        advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-8)
+        
+        action_probs = policy_net(rollout_states)
+        action_dist = Categorical(action_probs)
+        new_log_probs = action_dist.log_prob(rollout_actions)
+             
+        ratio = torch.exp(new_log_probs - old_log_probs)
+        noclip_term = advantage*ratio
+        clip_term = torch.clamp(ratio, min=1-eps, max=1+eps)*advantage
+        ppo_loss = -torch.min(noclip_term, clip_term).mean()
+        
+        policy_optimizer.zero_grad()
+        ppo_loss.backward()
+        policy_optimizer.step()
+        
+        
+        training_values = value_net(rollout_states).squeeze()
+        value_loss = mse_loss(training_values, rollout_returns)
+
+        value_optimizer.zero_grad()
+        value_loss.backward()
+        value_optimizer.step()
+        
+        old_policy_net = copy.deepcopy(policy_net)
+            
+        
+        if (n+1)%100==0:
+            eval_return = evaluate_policy(env, policy_net, device, num_episodes=5)
+            print(f"Iter: {n+1}, R: {eval_return}")
+            reward_totals.append(eval_return)
+    
+    print("Finished!\n")    
+    return reward_totals
+
+# def get_returns(rewards, gamma=0.99):
+#     returns = []
+#     for i in range(len(rewards)):
+#         R = 0
+#         for reward in reversed(rewards[i:]):
+#             R = reward + gamma * R
+#         returns.append(R)
+#     return returns
+
+def get_returns(rewards, dones, gamma=0.99):
+    returns = []
+    R = 0
+    for reward, done in zip(reversed(rewards), reversed(dones)):
+        if done:
+            R = 0
+        R = reward + gamma * R
+        returns.insert(0, R)
+    return returns
+
+def select_action(policy, state):
+    action_probs = policy(state)
+    action_dist = Categorical(action_probs)
+    action = action_dist.sample()
+    log_prob = action_dist.log_prob(action)
+    return action, log_prob
+
+def perform_rollout(env, actor, critic, T, device):
+    buffer = {
+        "state": [],
+        "action": [],
+        "reward": [],
+        "dones": [],
+        "old_log_probs": [],
+        "values": []
+        }
+    
+    state, info = env.reset()
+    state = torch.from_numpy(state).to(device)
+    
+    for _ in range(T):
+        with torch.no_grad():
+            action, log_prob = select_action(actor, state)
+            value = critic(state)
+            
+        next_state, reward, done, trunc, info = env.step(action.item())
+        next_state = torch.from_numpy(next_state).to(device)
+        
+        buffer["state"].append(state)
+        buffer["action"].append(action)
+        buffer["reward"].append(reward)
+        buffer["dones"].append(done or trunc)
+        buffer["old_log_probs"].append(log_prob)
+        buffer["values"].append(value)
+        
+        state = next_state
+        
+        if done or trunc:
+            state, info = env.reset()
+            state = torch.from_numpy(state).to(device)
+            
+    return buffer
+    
+def evaluate_policy(env, policy_net, device, num_episodes=5):
+    returns = []
+    for _ in range(num_episodes):
+        state, info = env.reset()
+        done = False
+        trunc = False
+        ep_return = 0
+        while not (done or trunc):
+            with torch.no_grad():
+                state_tensor = torch.tensor(state).to(device)
+                action_probs = policy_net(state_tensor)
+                action = torch.argmax(action_probs, dim=-1).item()
+            state, reward, done, trunc, info = env.step(action)
+            ep_return += reward
+        returns.append(ep_return)
+    return sum(returns) / len(returns)    
--- main.py
+++ main.py
@@ -4,22 +4,30 @@ import torch.optim as optim
 from env import DiceGame, run_episode, update_network
 from matplotlib import pyplot as plt
 from torch.distributions import Categorical
-import gym
-from learners import reinforce_learner, a2c_learner
+import gymnasium as gym
+from learners import reinforce_learner, a2c_learner, ppo_learner
 import numpy as np
 import optuna
+from plotly.io import show
+import gc
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 def objective(trial):
     
     params = {'episodes': 2000,
               'lr': trial.suggest_loguniform('lr', 1e-5, 1e-2),
               'gamma': trial.suggest_uniform('gamma', 0.8, 0.99),
-              'trunc': 200}
+              'trunc': 200,
+              'rollout_len': trial.suggest_int('rollout_len', 8, 64, 8),
+              'eps': trial.suggest_float('eps', 0.1, 0.3),
+              'update_epochs': 1,
+              'iterations': 5000}
     
-    print(f"lr: {params['lr']}\ngamma: {params['gamma']}")
-    
+    #print(f"lr: {params['lr']}\ngamma: {params['gamma']}")
+    print(params)
     env = gym.make('CartPole-v1', max_episode_steps = params.get("trunc"))
-    a2c_reward = a2c_learner(env, params, device)
+    a2c_reward = ppo_learner(env, params, device)
     return sum(a2c_reward)/len(a2c_reward)
 
 if __name__ == '__main__':
@@ -36,21 +44,28 @@ if __name__ == '__main__':
     # Set this to False to enable a full hyperparameter sweep with optun
     one_off = False
     
+    
     if one_off:
-       params = {'episodes': 2000,
-                 'lr': 1e-3,
-                 'gamma': 0.99,
-                 'trunc': 200}
+       params_of = {'episodes': 2000,
+                 'lr': 6e-4,
+                 'gamma': 0.85,
+                 'trunc': 200,
+                 'rollout_len': 16,
+                 'eps': 0.2,
+                 'update_epochs': 1,
+                 'iterations': 5000}
        
-       print(f"lr: {params['lr']}\ngamma: {params['gamma']}")
+       print(f"lr: {params_of['lr']}\ngamma: {params_of['gamma']}")
        
        # Make environment, run actor-critic learner, plot rewards over episodes
-       env = gym.make('CartPole-v1', max_episode_steps = params.get("trunc"))
-       a2c_reward = a2c_learner(env, params, device)
+       env = gym.make('CartPole-v1', max_episode_steps = params_of.get("trunc"))
+       a2c_reward = ppo_learner(env, params_of, device)
        plt.plot(a2c_reward)
        plt.show()
        
     else:
        # Run a hyperparameter sweep using params in the objective function
        study = optuna.create_study(direction='maximize')
-       study.optimize(objective, n_trials=10, gc_after_trial=True)
+       study.optimize(objective, n_trials=20, gc_after_trial=True)
+       fig = optuna.visualization.plot_intermediate_values(study)
+       show(fig)
